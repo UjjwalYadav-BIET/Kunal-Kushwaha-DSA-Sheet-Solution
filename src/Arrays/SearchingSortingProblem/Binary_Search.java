de019d4a280ad8f2f46d89aa6809b27505768141
UjjwalYadav-BIET <150013109+UjjwalYadav-BIET@users.noreply.github.com>
2025-09-13 21:09:35 +0530

    Updated something

--- src/Arrays/SearchingSortingProblem/Binary_Search.java
+++ src/Arrays/SearchingSortingProblem/Binary_Search.java
@@ -1,4 +1,10 @@
 package Arrays.SearchingSortingProblem;
 
+import java.util.Scanner;
+
 public class Binary_Search {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello world");
+    }
 }
