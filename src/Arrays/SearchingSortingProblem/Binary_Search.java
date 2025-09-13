eae9b8565b7c3e06b325018e362c2e9a82297aec
UjjwalYadav-BIET <150013109+UjjwalYadav-BIET@users.noreply.github.com>
2025-09-13 22:17:09 +0530

    your message

--- src/Arrays/SearchingSortingProblem/Binary_Search.java
+++ src/Arrays/SearchingSortingProblem/Binary_Search.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Binary_Search {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String s = in.next();
+        System.out.println(s);
         System.out.println("Hello world");
     }
 }
