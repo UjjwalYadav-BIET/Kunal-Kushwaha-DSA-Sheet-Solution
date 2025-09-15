56cc9225584e6834c48853edcb4ac50794ecea7c
UjjwalYadav-BIET <150013109+UjjwalYadav-BIET@users.noreply.github.com>
2025-09-15 22:40:03 +0530

    Your message

--- src/Arrays/SearchingSortingProblem/Binary_Search.java
+++ src/Arrays/SearchingSortingProblem/Binary_Search.java
@@ -4,9 +4,31 @@ import java.util.Scanner;
 
 public class Binary_Search {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
-        System.out.println(s);
-        System.out.println("Hello world");
+        int[] arr = {-12, -5, -1, 0, 4, 8, 9, 19};
+        int target = 9;
+        int ans = binary_Search(arr, target);
+        System.out.printf("Element %d is at position %d",target,ans);
     }
+
+    static int binary_Search(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // Finding the target element......
+            // int mid = (start+end)/2 this might show the error in the some cases because it can go beyond the limit of the integer type.
+            int mid = start + (end - start) / 2; /// that's why we use this formula.
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+
 }
