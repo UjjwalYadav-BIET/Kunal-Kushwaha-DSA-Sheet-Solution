db4603bf2ec1277a48a24e1d9e457dd6a8b6f6a6
UjjwalYadav-BIET <150013109+UjjwalYadav-BIET@users.noreply.github.com>
2025-09-17 13:38:03 +0530

    Your message

--- src/Arrays/SearchingSortingProblem/Linear_Search_Function.java
+++ src/Arrays/SearchingSortingProblem/Linear_Search_Function.java
@@ -1,10 +1,26 @@
 package Arrays.SearchingSortingProblem;
 
-import java.util.Scanner;
-
 public class Linear_Search_Function {
-    
+
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        int[] data = {10, 25, 3, 47, 8, 19, 32};
+        int valueToFind = 8;
+
+        int result = search(data, valueToFind);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
     }
 }
