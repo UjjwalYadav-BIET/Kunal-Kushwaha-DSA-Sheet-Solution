4de606cb66753515fdfa0ec3637ec91d96de844a
UjjwalYadav-BIET <150013109+UjjwalYadav-BIET@users.noreply.github.com>
2025-10-04 23:14:19 +0530

    Your message

---
+++ src/Arrays/SearchingSortingProblem/Bubble_Sort.java
@@ -0,0 +1,30 @@
+package Arrays.SearchingSortingProblem;
+
+import java.util.Arrays;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int arr[] = {1,2,4,5,6};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void bubble(int[] arr) {
+        boolean swap;
+        for (int i = 0; i < arr.length; i++) {
+            swap = false;
+            for (int j = 1; j < arr.length-i; j++) {
+                if (arr[j] < arr[j-1]) {
+                    // Now swap the numbers
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                break;
+            }
+        }
+    }
+}
